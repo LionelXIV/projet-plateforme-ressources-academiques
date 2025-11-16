3b5553dcc369aca71e6b24a5488093228e3340ef
AnaisBahloul <anais.bahloul2022@gmail.com>
2025-11-16 03:15:57 -0500

    Merge main into anais QUIZ IA , conflits réglé

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,5 @@
 import { useState, useEffect } from "react";
-<<<<<<< HEAD
 import { Plus, ClipboardList  } from "lucide-react";
-=======
-import { Plus } from "lucide-react";
->>>>>>> main
 import { Course, Document } from "./types";
 import Header from "./components/Header";
 import SearchFilters from "./components/SearchFilters";
@@ -13,10 +9,7 @@ import DocumentViewer from "./components/DocumentViewer";
 import AddCourseDialog from "./components/AddCourseDialog";
 import AboutSection from "./components/AboutSection";
 import Footer from "./components/Footer";
-<<<<<<< HEAD
 import QuizDialog from "./components/QuizDialog"; 
-=======
->>>>>>> main
 import {
   Pagination,
   PaginationContent,
@@ -29,10 +22,7 @@ import {
 import { apiFetch } from "./lib/api"
 
 export default function App() {
-<<<<<<< HEAD
   const [showQuizDialog, setShowQuizDialog] = useState(false);
-=======
->>>>>>> main
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState<string | null>(null);
   const [isAdmin, setIsAdmin] = useState(false);
